6cc942135f4879d81896ab1f158738ca0eabc83f
gustavofamorim <gustavofreitasamorim@gmail.com>
2018-12-23 18:13:57 -0200

    Updates

---
+++ precache-manifest.8621a44b09c1cd12a1383a35768c0404.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = [
+  {
+    "revision": "bc72967b876f0f6f48c8",
+    "url": "/js/filters.6c757053.js"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "f0fa65dbdf699cc5d924",
+    "url": "/css/my-sells.c1bae8d8.css"
+  },
+  {
+    "revision": "36d7bf1b9ae28b6bd9b9",
+    "url": "/css/app.5e168660.css"
+  },
+  {
+    "revision": "40f22b27fd6664f6e048",
+    "url": "/js/chunk-vendors.3f85b414.js"
+  },
+  {
+    "revision": "6132e2b9784a830621ef",
+    "url": "/css/config.751a9b05.css"
+  },
+  {
+    "revision": "6132e2b9784a830621ef",
+    "url": "/js/config.003fafd3.js"
+  },
+  {
+    "revision": "bc72967b876f0f6f48c8",
+    "url": "/css/filters.df536adb.css"
+  },
+  {
+    "revision": "8eb4006ca9c98fe9a540",
+    "url": "/js/analytics.6a7295a8.js"
+  },
+  {
+    "revision": "36d7bf1b9ae28b6bd9b9",
+    "url": "/js/app.211bb9bd.js"
+  },
+  {
+    "revision": "f0fa65dbdf699cc5d924",
+    "url": "/js/my-sells.a494c0ab.js"
+  },
+  {
+    "revision": "5fb01e20f43c7e7d7ea8",
+    "url": "/css/new.478414ed.css"
+  },
+  {
+    "revision": "5fb01e20f43c7e7d7ea8",
+    "url": "/js/new.771b6c8f.js"
+  },
+  {
+    "revision": "dbbc4b599cc2108ad15aa2f0479c2270",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8eb4006ca9c98fe9a540",
+    "url": "/css/analytics.5a7de1fe.css"
+  },
+  {
+    "revision": "417d3bbcb191974cf4b5ff34d1dabde7",
+    "url": "/CNAME"
+  }
+];
